796d3ce77b8294f61b5de73a67f297deacc5c524
armansabyr <armansabyrov@gmail.com>
2018-08-24 21:05:41 +0600

    finished

---
+++ src/main/java/kz/edu/nu/cs/exercise/State3.java
@@ -0,0 +1,22 @@
+package kz.edu.nu.cs.exercise;
+
+public class State3 extends State{
+    protected StateContext sc;
+    protected boolean accept = true;
+    //constructor
+    public State3 (StateContext sc) {
+      this.sc = sc;
+    }
+
+    public void actionA() {
+      this.sc.setCurrentState(this.sc.getState3());
+    }
+
+    public void actionB() {
+      this.sc.setCurrentState(this.sc.getState2());
+    }
+
+    public boolean isAccept() {
+        return this.accept;
+    }
+}

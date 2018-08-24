0f1c7aef3089c5ff21b3d9e5c593511845298e04
DiasDias97Dias <dias.duisembayev@nu.edu.kz>
2018-08-24 16:15:47 +0600

    Add files via upload

---
+++ src/main/java/kz/edu/nu/cs/exercise/State1.java
@@ -0,0 +1,9 @@
+package kz.edu.nu.cs.exercise;
+
+public class State1 extends State {
+
+	public State1(StateContext stateContext) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/kz/edu/nu/cs/exercise/State2.java
@@ -0,0 +1,9 @@
+package kz.edu.nu.cs.exercise;
+
+public class State2 extends State {
+
+	public State2(StateContext stateContext) {
+		// TODO Auto-generated constructor stub
+	}
+
+}

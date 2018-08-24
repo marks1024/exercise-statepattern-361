d5b1392a81935f531642e6799969c788a47ea381
yerlentoktarov <42671810+yerlentoktarov@users.noreply.github.com>
2018-08-24 18:13:47 +0600

    Add files via upload

---
+++ State1.java
@@ -0,0 +1,17 @@
+package kz.edu.nu.cs.exercise;
+
+public class State1 extends State{
+	public State1(StateContext stateContext) {
+		this.sc=stateContext;
+		this.accept=false;
+	}
+	public void actionA() {
+		sc.setCurrentState(sc.state2);
+	}
+	public void actionB() {
+		sc.setCurrentState(sc.state1);
+	}
+	public boolean isAccept() {
+		return this.accept;
+	}
+}

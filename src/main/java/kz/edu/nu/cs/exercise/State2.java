239a8ae6b40c5599557e645580cacafbf3c3d32a
Fandorin <aituar.kenges@nu.edu.kz>
2018-08-24 16:20:51 +0600

    Lab2-finished

--- src/main/java/kz/edu/nu/cs/exercise/State2.java
+++ src/main/java/kz/edu/nu/cs/exercise/State2.java
@@ -1,4 +1,18 @@
 package kz.edu.nu.cs.exercise;
 
-public class State2 {
+public class State2 extends State {
+    public State2(StateContext stateContext) {
+        sc = stateContext;
+    }
+    @Override
+    public void actionA() {
+        sc.setCurrentState(sc.state3);
+    }
+    @Override
+    public void actionB() {
+        sc.setCurrentState(sc.state1);
+    }
+    public boolean isAccept() {
+        return false;
+    }
 }

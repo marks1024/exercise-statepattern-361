938fe474517a826575017154788a1b94c9a3759e
This-is-MUHA <42671240+This-is-MUHA@users.noreply.github.com>
2018-08-24 17:13:56 +0600

    Add files via upload

---
+++ src/main/java/kz/edu/nu/cs/exercise/State3.java
@@ -0,0 +1,22 @@
+package kz.edu.nu.cs.exercise;
+
+public class State3 extends State {
+
+    protected boolean accept = true;
+
+    public State3(StateContext sc) {
+        this.sc = sc;
+    }
+
+    public void actionA() {
+        sc.setCurrentState(sc.state3);
+    }
+
+    public void actionB() {
+        sc.setCurrentState(sc.state2);
+    }
+
+    public boolean isAccept() {
+        return this.accept;
+    }
+}

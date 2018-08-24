0ce8c8d588b4eb758eeb2e600cda88ccfa282789
Alisher <alisher.kenzhebayev@nu.edu.kz>
2018-08-24 16:48:34 +0600

    Edit

--- src/test/java/exercise/TestStatePattern.java
+++ src/test/java/exercise/TestStatePattern.java
@@ -1,10 +1,12 @@
-package kz.edu.nu.cs.exercise;
+package exercise;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import exercise.StateContext;
 
 public class TestStatePattern {
 

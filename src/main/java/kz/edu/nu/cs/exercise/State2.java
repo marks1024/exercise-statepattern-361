3570bcfc1e474afb90726ffb3a0764be3c0bb83e
nurduman0210 <42678825+nurduman0210@users.noreply.github.com>
2018-08-25 00:34:11 +0545

    Add files via upload

---
+++ src/main/java/kz/edu/nu/cs/exercise/State2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kz.edu.nu.cs.exercise;
+
+/**
+ *
+ * @author admin
+ */
+public class State2 extends State {
+    
+    public State2(StateContext aThis) {
+    }
+   }
---
+++ src/main/java/kz/edu/nu/cs/exercise/State3.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kz.edu.nu.cs.exercise;
+
+/**
+ *
+ * @author admin
+ */
+public class State3 extends State{
+    
+    public State3(StateContext aThis) {
+    }
+}

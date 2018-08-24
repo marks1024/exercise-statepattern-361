e229135d8f0ca34c2c8994dcdca0725540698886
Imadra <rassul.khassen@nu.edu.kz>
2018-08-24 23:46:19 +0600

    working

---
+++ src/main/java/kz/edu/nu/cs/exercise/State1.java
@@ -0,0 +1,16 @@
+package kz.edu.nu.cs.exercise;
+
+public class State1 extends State {
+	public State1(StateContext k) {
+		this.sc = k;
+	}
+	@Override
+	public void actionA() {
+		sc.setCurrentState(sc.state2);
+    }
+	
+	@Override
+	public void actionB() {
+		sc.setCurrentState(sc.state1);
+    }
+}

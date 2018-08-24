8d7176aeba84b9e005728f0e8e31e5e457441cb8
VladimirDuisenbinov <42377096+VladimirDuisenbinov@users.noreply.github.com>
2018-08-24 17:20:45 +0600

    Add files via upload

---
+++ exercise-statepattern-361/src/main/java/kz/edu/nu/cs/exercise/StateContext.java
@@ -0,0 +1,35 @@
+package kz.edu.nu.cs.exercise;
+
+public class StateContext {
+    final State state1 = new State1(this);
+    final State state2 = new State2(this);
+    final State state3 = new State3(this);
+
+    private State currentState;
+
+    public StateContext() {
+        this.currentState = state1;
+    }
+ 
+    public void actionA() {
+    	currentState.actionA();
+    }
+
+    public void actionB() {
+    	currentState.actionB();
+    }
+
+    public boolean inAcceptState() {
+    	return currentState.isAccept();
+    }
+
+    public State getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(State currentState) {
+        this.currentState = currentState;
+    }
+    
+
+}

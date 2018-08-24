edf4805489ee4d4f92c74eb9c57405c22c0ec580
Mukhamejan Karatayev <mukhamejan.karatayev@nu.edu.kz>
2018-08-24 22:31:28 +0600

    First commit

---
+++ src/main/java/kz/edu/nu/cs/exercise/State1.java
@@ -0,0 +1,23 @@
+package kz.edu.nu.cs.exercise;
+
+public class State1 extends State {
+	
+	protected boolean accept = false;
+	
+	public State1(StateContext sc) {
+		this.sc = sc;
+	}
+	
+	public void actionA() {
+		sc.setCurrentState(sc.state2);
+	}
+
+	public void actionB() {
+		sc.setCurrentState(sc.getCurrentState());
+	}
+
+	public boolean isAccept() {
+		return accept;
+	}
+
+}

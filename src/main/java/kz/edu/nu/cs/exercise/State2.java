8bb6e0f69ddb91a04e827f0de40e73739d32eaaa
Dastan <idastan97@gmail.com>
2018-08-24 20:32:16 -0300

    first

---
+++ src/main/java/kz/edu/nu/cs/exercise/State2.java
@@ -0,0 +1,19 @@
+package kz.edu.nu.cs.exercise;
+
+public class State2 extends State {
+	
+	public State2(StateContext sc) {
+		this.sc = sc;
+	}
+	
+	@Override
+	public void actionA() {
+		sc.setCurrentState(sc.state3);
+    }
+	
+	@Override
+    public void actionB() {
+		sc.setCurrentState(sc.state1);
+    }
+
+}

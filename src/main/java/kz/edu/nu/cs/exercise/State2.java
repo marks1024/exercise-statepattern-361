477a1027ba3c2ee7d6a536b6d99f8eceea9f9b1f
Dead Inside <b.mukazhan@gmail.com>
2018-08-24 17:30:37 +0600

    100% works

---
+++ src/main/java/kz/edu/nu/cs/exercise/State2.java
@@ -0,0 +1,24 @@
+package kz.edu.nu.cs.exercise;
+
+public class State2 extends State {
+
+
+    protected StateContext sc;
+    protected boolean accept = false;
+
+    public State2(StateContext stateContext) {
+        sc = stateContext;
+    }
+
+    public void actionA() {
+        sc.setCurrentState(sc.state3);
+    }
+
+    public void actionB() {
+        sc.setCurrentState(sc.state1);
+    }
+
+    public boolean isAccept() {
+        return this.accept;
+    }
+}
